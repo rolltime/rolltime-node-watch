4bcc644d06ea7c73047c90070da6d90d34a2f42a
Luis Capelo <luiscape@gmail.com>
2015-09-02 22:56:33 -0400

    adding a mongo configure script

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 #
-# Configure database.
+# Creates an user in the
+# MongoDB instance.
 #
-echo "Nothing here yet."
+mongo $MONGO_PORT_27017_TCP_ADDR:27017/$MONGODB_DATABASE \
+  --eval "db.createUser({user: '$MONGODB_USER_NAME', pwd: '$MONGODB_USER_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGODB_DATABASE' },]})"
