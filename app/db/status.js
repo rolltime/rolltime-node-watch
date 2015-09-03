b15497f6b8e8ca972aab5a46d60c831ca697fa2c
Luis Capelo <luiscape@gmail.com>
2015-09-02 20:53:11 -0400

    linting

--- app/db/status.js
+++ app/db/status.js
@@ -2,11 +2,11 @@
 var mongoose = require('mongoose')
 
 // define the schema for our user model
-var ScraperStatus = mongoose.Schema({
+var CollectorStatus = mongoose.Schema({
   id: String,
   status: String,
   message: String,
   time: String
 })
 
-module.exports = mongoose.model('User', ScraperStatus)
+module.exports = mongoose.model('User', CollectorStatus)
